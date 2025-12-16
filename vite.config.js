76ccc5038027ed4d5c2f89596f0b7f37bf76cd63
w2334681436-wang <w2334681436@gmail.com>
2025-12-16 09:35:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+// 1. 引入 package.json
+import packageJson from './package.json'
 
 export default defineConfig({
+  // 2. 定义全局变量
+  define: {
+    // JSON.stringify 是必须的，因为我们要注入的是一个字符串常量
+    '__APP_VERSION__': JSON.stringify(packageJson.version),
+  },
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: '错题本',      // [修改] 名称改为通用
-        short_name: '错题本', // [修改]
-        description: '全科目错题沉浸式复盘工具', // [修改]
+        name: '错题本',
+        short_name: '错题本',
+        description: '全科目错题沉浸式复盘工具',
         theme_color: '#ffffff',
         background_color: "#ffffff",
         display: 'standalone',
